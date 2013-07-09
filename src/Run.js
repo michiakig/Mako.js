77563a7224c325cedfe937329c62367368c96cec
aki <spacemanaki@gmail.com>
2013-07-09 18:30:50 -0400

    clean up entry point a bit. added "usage"

--- src/Run.js
+++ src/Run.js
@@ -1,7 +1,15 @@
+// Reads a ROM in and runs it
 if(require.main === module) {
-    var Read = require("./Read.js");
-    var Mako = require("./MakoVM.js");
-    var rom = Read.readMakoRom(process.argv[2]);
-    var vm = new Mako.MakoVM(rom);
+    if(process.argv.length <= 2) {
+        console.log("Usage: node Run.js [rom filename]");
+        process.exit(1);
+    }
+
+    var readMakoRom = require("./Read.js").readMakoRom;
+    var MakoVM = require("./MakoVM.js").MakoVM;
+
+    var filename = process.argv[2];
+    var rom = readMakoRom(filename);
+    var vm = new MakoVM(rom);
     vm.run();
 }
